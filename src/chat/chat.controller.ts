ecf025f66ddc892410362c052c433777a56a6702
zenos <zenos.chen@foxmail.com>
2025-05-11 21:24:48 +0800

    fix bug

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -297,7 +297,7 @@ export class ChatController {
       if (!data.roomId) throw new Error('roomId is required');
       if (!data.type) throw new Error('type is required');
       if (data?.type !== 'group') throw new Error('type must be group');
-      const { includeBlocked = false } = data;
+      const { includeBlocked = true } = data;
       const res = await this.chatService.getGroupMembersInfo({
         roomId: data.roomId,
         userId: req.user.id,
